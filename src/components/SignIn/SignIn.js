805483cdcb31c2c2b353b08f6a2f9a1bc2e72260
subhankarCN <subhankar.ray@capitalnumbers.com>
2021-05-07 20:35:33 +0530

    check auto deploy

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -91,7 +91,7 @@ export default function SignIn(props) {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Sign in 1
         </Typography>
         <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <TextField
